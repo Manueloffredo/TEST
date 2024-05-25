9643b93d60a9a18689c8a64bd419d55c7e1dd787
galie <galie@Alex>
2024-05-25 18:12:32 +0200

    secondo commit

--- ProgettoIS/src/Database/StudenteDAO.java
+++ ProgettoIS/src/Database/StudenteDAO.java
@@ -1,5 +1,61 @@
 package Database;
 
-public class StudenteDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import Entity.EntityStudente;
+import database.DBManager;
+import entity.CartaCredito;
+import entity.Cliente;
+import exception.DAOException;
+import exception.DBConnectionException;
+
+public class StudenteDAO {
+	
+	public static EntityStudente readStudente(String username, String password) {
+		
+	}
 }
+
+/*
+	public static Cliente createCliente(String username, String password, String numeroTelefono, CartaCredito cartaCredito) throws DBConnectionException, DAOException{
+		
+		String query = "INSERT INTO Clienti VALUES (?,?,?,?);";
+		Cliente cliente = null;
+		
+		try {
+
+			Connection connection = DBManager.getConnection();
+
+			try {
+				
+				PreparedStatement statement = connection.prepareStatement(query);
+				statement.setString(1, username);
+				statement.setString(2, password);
+				statement.setString(3, numeroTelefono);
+				statement.setString(4, cartaCredito.name()); 
+				
+				statement.executeUpdate();
+				
+				cliente = new Cliente(username, password, numeroTelefono, cartaCredito);
+
+			} catch (SQLException e) {
+
+				throw new DAOException("Creazione cliente non riuscita");
+
+			} finally {
+
+				DBManager.closeConnection();
+			}
+
+		} catch (SQLException e) {
+
+			throw new DBConnectionException("Connessione al Database non riuscita");
+
+		}
+		
+		return cliente;
+					
+	}
+*/
--- ProgettoIS/src/Entity/EntitySegreteriaStudenti.java
+++ ProgettoIS/src/Entity/EntitySegreteriaStudenti.java
@@ -1,5 +1,32 @@
 package Entity;
 
-public class EntitySegreteriaStudenti {
+import java.util.List;
 
+public class EntitySegreteriaStudenti {
+	private List<EntityCorso> corsi;
+	private List<EntityDocente> listaDocenti;
+	
+	public EntitySegreteriaStudenti(List<EntityCorso> corsi, List<EntityDocente> listaDocenti) {
+		super();
+		this.corsi = corsi;
+		this.listaDocenti = listaDocenti;
+	}
+
+	public List<EntityCorso> getCorsi() {
+		return corsi;
+	}
+
+	public void setCorsi(List<EntityCorso> corsi) {
+		this.corsi = corsi;
+	}
+
+	public List<EntityDocente> getListaDocenti() {
+		return listaDocenti;
+	}
+
+	public void setListaDocenti(List<EntityDocente> listaDocenti) {
+		this.listaDocenti = listaDocenti;
+	}
+	
+	
 }
