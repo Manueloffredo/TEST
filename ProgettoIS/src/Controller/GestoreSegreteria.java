b4d8186000a8dc74342ad8be9f4ed55c634d80ab
galie <galie@Alex>
2024-05-26 13:20:40 +0200

    gestore segr

--- ProgettoIS/src/Controller/GestoreSegreteria.java
+++ ProgettoIS/src/Controller/GestoreSegreteria.java
@@ -1,5 +1,59 @@
 package Controller;
 
-public class GestoreSegreteria {
+import java.util.ArrayList;
+import java.util.List;
 
+import Database.StudenteDAO;
+import Entity.EntityStudente;
+import exception.DAOException;
+import exception.DBConnectionException;
+import exception.OperationException;
+
+
+
+public class GestoreSegreteria {
+	
+	private static GestoreSegreteria gS = null;
+	
+	public static GestoreSegreteria getIstance() {
+		
+		if( gS == null)		gS = new GestoreSegreteria();
+		
+		return gS;
+	}
+	
+	public boolean login(String Username, String Password) throws OperationException {
+		
+		try {
+			
+			EntityStudente studente = StudenteDAO.readStudente(username, password);
+			
+			if(Username.equals(studente.getUsername()) && Password.equals(studente.getPassword())){
+				
+				return true;
+				
+			} else {
+				
+				throw new OperationException("Credenziali non valide");
+			}
+		
+		}catch(DBConnectionException dbEx) {
+			
+			throw new OperationException("\nRiscontrato problema interno applicazione!\n");
+
+		}catch(DAOException ex) {
+
+			throw new OperationException("Ops, qualcosa e' andato storto..");
+		}
+		
+	}
+	//public static List<String> visualizzaEsamisvolti();
+	//public void inserisciEsami();
+	
+	
 }
+
+
+/*
+
+*/
